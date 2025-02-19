054dc4524a78b5e8015226ffa62e20dcc11f0004
Kipsang Evans <ekipsang200@gmail.com>
2025-02-19 02:27:29 -0800

    around the corner

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,28 @@
+import threading
 from django.shortcuts import render, redirect
-from .models import Phrase
+from django.core.mail import send_mail
+from django.conf import settings
+
+def send_email_async(subject, message, recipient_list):
+    """Function to send email in a separate thread."""
+    thread = threading.Thread(
+        target=send_mail,
+        args=(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list),
+        kwargs={'fail_silently': False}
+    )
+    thread.start()  # Run the email sending process in a new thread
 
 def index(request):
     if request.method == 'POST':
         phrase_text = request.POST.get('phrase')
-        if phrase_text:  # Make sure input is not empty
-            Phrase.objects.create(phrase=phrase_text)  # Save phrase
-        return redirect('success')  # Redirect to clear form and avoid resubmission
+        if phrase_text:  # Ensure input is not empty
+            # Send email asynchronously
+            send_email_async(
+                subject="New Phrase Submission",
+                message=f"A new phrase was submitted:\n\n{phrase_text}",
+                recipient_list=['ekipsang200@gmail.com']
+            )
 
-    # Fetch all saved phrases
-    phrases = Phrase.objects.all().order_by('-id')
+        return redirect('success')  # Redirect after submission
 
-    return render(request, 'index.html', {'phrases': phrases})
+    return render(request, 'index.html')  # Render the form page
--- pi/settings.py
+++ pi/settings.py
@@ -158,3 +158,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Use your email provider (Gmail, Outlook, etc.)
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ekipsang200@gmail.com'  # Your email address
+EMAIL_HOST_PASSWORD = 'niqy dwbv xkwl byig'  # App password (not your login password)
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
